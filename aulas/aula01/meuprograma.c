6b1a89b87eef5eb00bd8555406646ceca17f1da5
ottoni1504 <25529756-ottoni1504@users.noreply.replit.com>
2023-08-24 00:46:11 +0000

    arquivos da aula 01

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+ printf("Hello World!\n");
+ printf("A raiz quadrada de 2 é %f\n", sqrt(2));
+  return 0;
+}
+
+
+/*
+Autor: Rafael Games Ramos
+Programa revolucionario que diz "Hello World"
+Data: 23/08/23
+*/
---
+++ praticas/pratica01/pratica01.c
@@ -0,0 +1,24 @@
+/*
+Faça um programa em C que calcule a média final a partir da fórmula (0,4 x A1) + (0,6 x A2).
+Considere A1 e A2 números reais entre 0 a 10.
+*/
+
+#include <stdio.h>
+
+int main() {
+float nota_a1 = 5.5f;
+float nota_a2 = 4.0f;
+
+float media = 0.4 * nota_a1 + 0.6 * nota_a2;
+
+printf ("A média final é %f\n", media);
+
+  if(media <= 0.5) {
+   printf ("Você passou de semestre!\n");
+    
+  } else{
+   printf ("Você está de recuperação...");
+  }
+  
+  return 0;
+}
